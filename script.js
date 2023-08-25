ff720b9ee6bcfe08a3c26bac6df243e15f7e1762
joaoxmarcello <joaomarcello869@gmail.com>
2023-08-25 15:01:06 -0300

    First commit JS

---
+++ script.js
@@ -0,0 +1,24 @@
+// criar uma função livro com 3 paramentros: nome, ano e autor.
+// passsar no corpo da função nome para toUppercase()
+// calcular o total de anos desde o lançamento do livro 2023 - ano.
+// criar uma variável com a frase: nome + "por" + autor;
+// colocar os 3 valores anteriores em um objeto
+// retornar o objeto definido
+// executar a funcao com os seguintes argumentos:
+// "O senhor dos Anéis", "1954", "J. R. R. Tolkien"
+// Guardar o retorno da função em uma variável 
+// log a frase final da função executada no console 
+
+function livro(nome, ano, autor) {
+    const nomeGrande = nome.toUpperCase();
+    const totalAnos = 2023 - ano;
+    const frase = nome + " por " + autor;
+    const objeto = {
+    nome: nomeGrande,
+    totalAnos,
+    frase,
+   }
+   return objeto;
+ }
+const Retorno = livro("O senhor dos Anéis", 1954 ,"J. R. R. Tolkien");
+console.log(Retorno.frase);
